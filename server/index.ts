769a68752d9b29d2a6416c54270b4d52e62c81cc
Sewanou Landjeli <sewanou@Sewanous-MBP.lan>
2025-04-02 03:30:03 +0200

    devfolio

--- server/index.ts
+++ server/index.ts
@@ -47,21 +47,15 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 5000;
-  // Simplifiez la configuration d'écoute pour une meilleure compatibilité en local
-  server.listen(port, "localhost", () => {
-    log(`serving on http://localhost:${port}`);
+  // Utilisation de process.env.PORT pour Render
+  const port = process.env.PORT || 5000;
+  server.listen(port, "0.0.0.0", () => {
+    log(`serving on http://0.0.0.0:${port}`);
   });
 })();
